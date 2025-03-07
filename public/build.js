976f4310f0ff3dbfb33863111ab6d16f8a0fd007
srad <ssedighi@posteo.de>
2025-03-07 12:49:03 +0100

    remove files

--- public/build.js
+++
@@ -1,2 +0,0 @@
-window.APP_BUILD = "dev-build";
-window.APP_VERSION = "1.0.0-alpha";
--- public/env.js
+++
@@ -1,5 +0,0 @@
-window.APP_APIURL = "http://192.168.0.219:3000/api/v1";
-window.APP_BASE = "http://192.168.0.219:3000";
-window.APP_NAME = "StreamSink";
-window.APP_SOCKETURL = "ws://192.168.0.219:3000/api/v1/ws";
-window.APP_FILEURL = "http://192.168.0.219:4000";
